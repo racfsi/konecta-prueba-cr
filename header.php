a885af7c4d5de38672a671d1e92782c5bce6e96b
racfsi <ruizmunozc@gmail.com>
2023-10-25 19:39:37 -0500

    Ajuste header, atributo title

--- header.php
+++ header.php
@@ -1,13 +1,11 @@
 <?php
 ?>
-
 <!DOCTYPE html>
 
 <head>
     <meta charset="utf-8">
     <title>Coffe Konecta</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="assets/css/bootstrap.css" />
